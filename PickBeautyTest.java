997f13a9391a978bf4cea897bb8defd85e17bb00
EZHfk <71257992+EZHfk@users.noreply.github.com>
2020-09-25 04:26:27 -0500

    Add files via upload

---
+++ PickBeautyTest.java
@@ -0,0 +1,22 @@
+
+public class PickBeautyTest {
+
+	public boolean testAdd() {
+		return false;
+	}
+	public boolean testRemove() {
+		return false;
+	}
+	public boolean testGet() {
+		return false;
+	}
+	public boolean tesList() {
+		return false;
+	}
+	public boolean testSearch() {
+		return false;
+	}
+	public boolean testNext() {
+		return false;
+	}
+}
